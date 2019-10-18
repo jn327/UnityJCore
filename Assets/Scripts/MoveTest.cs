ae526f054607eb282f9a6f1f79e18ccfe486ada3
Josh <joshuanewland33@hotmail.com>
2019-10-18 14:40:38 +0100

    Think i just updated to new unity version

---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour 
+{
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		MoveForwardToTarget( transform.right, Time.deltaTime, target.transform );
+	}
+
+	void MoveForwardToTarget(Vector3 forwardVelocity, float deltaTime, Transform target)
+	{
+		float distanceLeft = (target.position - transform.position).magnitude;
+		float deltaDist = forwardVelocity.magnitude * deltaTime;
+
+		if (distanceLeft < deltaDist)
+		{
+			//forwardVelocity = forwardVelocity * distanceLeft / deltaDist;
+			//forwardVelocity = forwardVelocity * distanceLeft;
+			transform.position = target.position;
+
+			Debug.DrawRay( transform.position, forwardVelocity, Color.red, deltaTime );
+			//Debug.Break();
+		}
+		else
+		{
+			Debug.DrawRay( transform.position, forwardVelocity, Color.green, deltaTime );
+			transform.position += forwardVelocity * deltaTime;
+		}
+
+		//transform.position += forwardVelocity * deltaTime;
+	}
+}
