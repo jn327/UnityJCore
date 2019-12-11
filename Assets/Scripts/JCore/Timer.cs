8353c5dbde746ff91a869fd546784de61a094fd1
Josh <joshuanewland33@hotmail.com>
2019-12-11 13:01:11 +0000

    super simple timer class

---
+++ Assets/Scripts/JCore/Timer.cs
@@ -0,0 +1,38 @@
+﻿public class Timer
+{
+    public float remainingTime 
+    {
+        get;
+        private set;
+    }
+
+    private event System.Action _onComplete;
+
+    public Timer( float duration )
+    {
+        remainingTime = duration;
+    }
+
+    public void Tick(float deltaTime)
+    {
+        //if it's already reached 0 do nothing.
+        if (remainingTime <= 0)
+        {
+            return;
+        }
+
+        //count down remaning time.
+        remainingTime -= deltaTime;
+
+        //if we've reached 0 then send off our complete event.
+        if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+
+            if (_onComplete != null)
+            {
+                _onComplete.Invoke();
+            }
+        }
+    }
+}
